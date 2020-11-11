65a6ac09f1a3389f95e020f13c1ebf008bc94669
itwell <822210260@qq.com>
2020-11-11 17:48:54 +0800

    新建server模块

---
+++ server/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,27 @@
+package com.course.system.domain;
+
+/**
+ * @author itwell
+ * @date 2020-11-11 16:32
+ */
+public class Test {
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
