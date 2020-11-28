2ba880e91fcfe9aa2db95b1b47ed3c074fd53016
itwell <822210260@qq.com>
2020-11-28 22:30:59 +0800

    讲师头像上传优化

--- file/src/main/java/com/course/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/course/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.course.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //我访问 http://127.0.0.1:9003/file/f/teacher/8nZoCcDs-avator2.jpg
         //形成的路径就是 Z:/course/file/src/main/resources/images/course/teacher/8nZoCcDs-avator2.jpg
-        registry.addResourceHandler("/f/**").addResourceLocations("file:Z:/course/file/src/main/resources/images/course/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
     }
 }
--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -5,6 +5,7 @@ import com.course.server.dto.ResponseDto;
 import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,14 @@ import java.io.IOException;
 public class UploadController {
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
+    public static final String BUSINESS_NAME = "文件上传";
+
+    @Value("${file.domain}")
+    private String FILE_DOMAIN;
+
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @RequestMapping("/upload")
     public ResponseDto test(@RequestParam MultipartFile file) throws IOException {
         logger.info("上传文件开始:{}",file);
@@ -32,12 +41,14 @@ public class UploadController {
         //保存文件到本地
         String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = "Z:/course/file/src/main/resources/images/course/teacher/" + key + "-" + fileName;
+        String fullPath = FILE_PATH + "teacher/" + key + "-" + fileName;
         File dest = new File(fullPath);
         file.transferTo(dest);
 
         logger.info(dest.getAbsolutePath());
+
         ResponseDto responseDto = new ResponseDto();
+        responseDto.setContent(FILE_DOMAIN + "f/teacher/" + key + "-" + fileName);
         return responseDto;
     }
 }
