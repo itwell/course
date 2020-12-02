f650686b5dd9beeddfcf03cf5ce749eb2aaeeb96
itwell <822210260@qq.com>
2020-12-02 19:25:45 +0800

    合并分片文件

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -40,7 +40,7 @@ public class UploadController {
     private FileService fileService;
 
     @RequestMapping("/upload")
-    public ResponseDto test(@RequestBody FileDto fileDto) throws IOException {
+    public ResponseDto test(@RequestBody FileDto fileDto) throws Exception {
         logger.info("上传文件开始");
         String use = fileDto.getUse();
         String key = fileDto.getKey();
@@ -60,13 +60,19 @@ public class UploadController {
 
         //不同操作系统File.separator 目录分隔符
         //String path = dir + File.separator + key + "." + suffix + "." + fileDto.getShardIndex();
+
         String path = new StringBuffer(dir)
-                        .append(File.separator)
-                        .append(key).append(".")
-                        .append(suffix)
-                        .append(".")
-                        .append(fileDto.getShardIndex()).toString();
-        String fullPath = FILE_PATH + path;
+                .append(File.separator)
+                .append(key).append(".")
+                .append(suffix)
+                .toString();    //不带.1
+
+        String localPath = new StringBuffer(path)
+                .append(".")
+                .append(fileDto.getShardIndex())
+                .toString();    //带.1
+
+        String fullPath = FILE_PATH + localPath;
         File dest = new File(fullPath);
         shard.transferTo(dest);
         logger.info(dest.getAbsolutePath());
@@ -78,30 +84,33 @@ public class UploadController {
         ResponseDto responseDto = new ResponseDto();
         fileDto.setPath(FILE_DOMAIN + path);
         responseDto.setContent(fileDto);
+
+        if (fileDto.getShardIndex().equals(fileDto.getShardTotal())){
+            this.merge(fileDto);
+        }
+
         return responseDto;
     }
 
-    @GetMapping("/merge")
-    public ResponseDto merge() throws Exception {
-        File newFile = new File(FILE_PATH + "/course/test123.mp4");
+    public void merge(FileDto fileDto) throws Exception {
+        logger.info("合并分片开始======================>");
+        String path = fileDto.getPath();    //http://127.0.0.1:9000/file/f/course\6sfSqfOwzmik4A4icMYuUe.mp4
+        path = path.replace(FILE_DOMAIN, "");   //course\6sfSqfOwzmik4A4icMYuUe.mp4
+        Integer shardTotal = fileDto.getShardTotal();   //
+        File newFile = new File(FILE_PATH + path);
         FileOutputStream outputStream = new FileOutputStream(newFile, true);
         FileInputStream fileInputStream = null; //分片文件
         byte[] byt = new byte[10 * 1024 * 1024];
         int len;
 
         try {
-            //读取第一个分片
-            fileInputStream = new FileInputStream(new File(FILE_PATH + "/course/Bc05XtFn.blob"));
+            for (int i = 0; i < shardTotal; i++) {
+                //读取第i个分片
+                fileInputStream = new FileInputStream(new File(FILE_PATH + path + "." + (i + 1)));
 
-            while ((len = fileInputStream.read(byt)) != -1) {
-                outputStream.write(byt, 0, len);
-            }
-
-            //读取第二个分片
-            fileInputStream = new FileInputStream(new File(FILE_PATH + "/course/roQbPm2x.blob"));
-
-            while ((len = fileInputStream.read(byt)) != -1) {
-                outputStream.write(byt, 0, len);
+                while ((len = fileInputStream.read(byt)) != -1) {
+                    outputStream.write(byt, 0, len);
+                }
             }
         } catch (IOException e) {
             logger.error("分片合并异常", e);
@@ -116,8 +125,6 @@ public class UploadController {
                 logger.error("IO流关闭", e);
             }
         }
-
-        ResponseDto responseDto = new ResponseDto();
-        return responseDto;
+        logger.info("合并分片结束======================>");
     }
 }
