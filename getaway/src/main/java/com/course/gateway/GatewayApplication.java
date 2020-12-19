1298aab1d05027aaad2f68a455f0f90054a3bcc8
itwell <822210260@qq.com>
2020-12-19 10:48:32 +0800

    用户管理与登录:实现数据包登陆拦截功能

--- getaway/src/main/java/com/course/gateway/GatewayApplication.java
+++ getaway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
@@ -14,6 +15,7 @@ import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan
 public class GatewayApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
