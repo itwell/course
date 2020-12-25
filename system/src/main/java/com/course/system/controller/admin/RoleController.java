c96fab7b19452e81f2688f0bd5c2e654068ad7c0
itwell <822210260@qq.com>
2020-12-26 00:09:35 +0800

    通用权限管理: 打开用户模态框时,加载角色用户关联数据

--- system/src/main/java/com/course/system/controller/admin/RoleController.java
+++ system/src/main/java/com/course/system/controller/admin/RoleController.java
@@ -116,4 +116,17 @@ public class RoleController {
         return responseDto;
     }
 
+    /**
+     * 加载用户
+     * @param roleId
+     */
+    @GetMapping("/list-user/{roleId}")
+    public ResponseDto listUser(@PathVariable String roleId) {
+        logger.info("加载用户开始");
+        ResponseDto responseDto = new ResponseDto<>();
+        List<String> userIdList = roleService.listUser(roleId);
+        responseDto.setContent(userIdList);
+        return responseDto;
+    }
+
 }
