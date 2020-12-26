c58acf70cf2064709abf8e8d3bc04981ceeaed7c
itwell <822210260@qq.com>
2020-12-26 14:34:49 +0800

    前端界面权限拦截,增加路由权限判断

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -574,13 +574,24 @@
             $.getScript('/ace/assets/js/ace.min.js');
 
             _this.loginUser = Tool.getLoginUser();
+
+            //进入这个页面时,检查是否有打开这个页面的权限
+            if (!_this.hasResourceRouter(_this.$route.name)) {
+                _this.$router.push("/login");
+            }
         },
         watch: {
             $route: {
                 handler:function(val, oldVal){
                     //激活侧边栏状态写法2
-                    // console.log("---->页面跳转：", val, oldVal);
+                    console.log("---->页面跳转：", val, oldVal);
                     let _this = this;
+
+                    if (!_this.hasResourceRouter(val.name)) {
+                        _this.$router.push("/login");
+                        return;
+                    }
+
                     _this.$nextTick(function(){  //页面加载完成后执行
                         _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
                     })
@@ -629,6 +640,24 @@
              */
             hasResource(id) {
                 return Tool.hasResource(id);
+            },
+
+            /**
+             * 查找是否有权限
+             * @param router
+             */
+            hasResourceRouter(router) {
+                let _this = this;
+                let resources = Tool.getLoginUser().resources;
+                if (Tool.isEmpty(resources)) {
+                    return false;
+                }
+                for (let i = 0; i < resources.length; i++) {
+                    if (router === resources[i].page) {
+                        return true;
+                    }
+                }
+                return false;
             },
         }
     }
