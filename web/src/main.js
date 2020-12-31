2756ee4980720fcb4141741da5250f81b79927da
itwell <822210260@qq.com>
2020-12-31 23:23:13 +0800

    网站首页开发:     增加过滤器     增加card等级显示

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import filter from './filter/filter'
 import axios from 'axios'
 
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = axios;
+
+//全局过滤器
+Object.keys(filter).forEach(key => {
+    Vue.filter(key,filter[key])
+});
 
 new Vue({
     router,
